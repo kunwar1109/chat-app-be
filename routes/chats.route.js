d5e8f292612efa0dc77969de8cd4286f2117a492
kunwar1109 <161008465+kunwar1109@users.noreply.github.com>
2024-09-06 17:18:47 +0530

    fetching chat and chat details feature added

--- routes/chats.route.js
+++ routes/chats.route.js
@@ -1,16 +1,49 @@
 import express from "express";
-
+import mongoose from "mongoose";
+import { Chat } from "../models/chats.model.js";
 const chatRouter = express.Router();
 
 //create a chat
-chatRouter.route("/").post(async (req, res) => {});
+chatRouter
+  .route("/")
+  .get(async (req, res) => {
+    const { userId } = req.body;
+    try {
+      const chats = await Chat.find({
+        members: userId,
+      });
+
+      res.status(200).json({
+        success: true,
+        chats,
+      });
+    } catch (error) {
+      res.status(400).json({ success: false, message: error.message });
+    }
+  })
+  .post(async (req, res) => {});
 
 //get chat details and update
 chatRouter
   .route("/:chatId")
   .get(async (req, res) => {
     const { chatId } = req.params;
-    res.send(chatId);
+    try {
+      const chat = await Chat.findById(chatId);
+      if (chat) {
+        res.status(200).json({
+          success: true,
+          chat,
+        });
+      } else {
+        res.status(404).json({
+          success: false,
+          message: "Chat Not Found",
+        });
+      }
+    } catch (error) {
+      res.status(400).json({ success: false, message: error.message });
+    }
   })
   .put(async (req, res) => {});
 
