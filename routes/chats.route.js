d36380442361f082fc27dea2c11a404da2e13356
kunwar1109 <161008465+kunwar1109@users.noreply.github.com>
2024-09-07 12:54:17 +0530

    create a chat feature

--- routes/chats.route.js
+++ routes/chats.route.js
@@ -2,7 +2,7 @@ import express from "express";
 import { Chat } from "../models/chats.model.js";
 const chatRouter = express.Router();
 
-//create a chat
+//create  a chat and get chats for a user
 chatRouter
   .route("/")
   .get(async (req, res) => {
@@ -20,7 +20,19 @@ chatRouter
       res.status(400).json({ success: false, message: error.message });
     }
   })
-  .post(async (req, res) => {});
+  .post(async (req, res) => {
+    const chat = req.body;
+    try {
+      const NewChat = new Chat(chat);
+      await NewChat.save();
+      return res.status(201).json({
+        success: true,
+        chat: NewChat,
+      });
+    } catch (error) {
+      res.status(400).json({ success: false, message: error.message });
+    }
+  });
 
 //get chat details and update
 chatRouter
@@ -44,6 +56,10 @@ chatRouter
       res.status(400).json({ success: false, message: error.message });
     }
   })
-  .put(async (req, res) => {});
+  .put(async (req, res) => {
+    const { chatId } = req.params;
+    try {
+    } catch (error) {}
+  });
 
 export { chatRouter };
