c8cd30a25120b10df081b0ddf3cf99f1643873e5
kunwar1109 <161008465+kunwar1109@users.noreply.github.com>
2024-09-18 21:26:32 +0530

    added socket functions

--- namespaces/chat.namespace.js
+++ namespaces/chat.namespace.js
@@ -1,11 +1,23 @@
 export default (chatNsp) => {
   chatNsp.on("connection", (socket) => {
-    console.log("A user connected to the chat namespace");
+    console.log("A user connected to the chat namespace", socket.id);
 
-    //join the room (with chatid as room)
+    //join the room (with chatid as roomName)
+    socket.on("join_chat", (chatId) => {
+      socket.join(chatId);
+      console.log(`socket ${socket.id} joined`);
+    });
 
     //broadcast message to room except to one who sent
+    socket.on("send_msg", ({ message }) => {
+      socket.broadcast.to(message.chatId).emit("recieve_msg", message, () => {
+        console.log("hello");
+      });
+    });
 
-    //
+    //leave room function
+    socket.on("leave_chat", (chatId) => {
+      socket.leave(chatId);
+    });
   });
 };
